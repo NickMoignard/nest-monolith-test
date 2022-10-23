38ed962f5ceef79630d7cd8e687b008c79840cc4
Nick Moignard <nicklmoignard@gmail.com>
2022-10-24 10:20:15 +1100

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Monolith')
+    .setDescription('The Monolith API description')
+    .setVersion('1.0')
+    .addTag('monolith')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
